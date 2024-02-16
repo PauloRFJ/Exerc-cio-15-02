9e4fc6cb863b85e7671118ff336d5af88b8c0d55
Roberto <roberto220488@gmail.com>
2024-02-15 21:50:01 -0300

    Correção Yan

--- assets/script.js
+++ assets/script.js
@@ -4,28 +4,59 @@
 
 //A execução do algoritmo deve ser contínua e o número 0 ou ‘sair’ é a flag para parar a execução.
 
-console.log('main.js')
-let continua = true;
-while (continua) {
-    const input = prompt('Digite nome ou código do produto. Digite 0 ou sair para encerrar')    
-    switch (input.toLocaleLowerCase()) {
+const inputProduto = document.getElementById('produto')
+
+const respostah4 = document.getElementById('resposta')
+
+function consultarPreco() {
+    // transformar para minusculo e remover acentos.
+    const produto = inputProduto.value.toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+    const resposta = precos(produto)
+    respostah4.innerText = resposta
+}
+
+function precos(produto) {
+    switch (produto) {
         case 'morango':
         case '1':
-            alert('R$ 5,99')
-            break;
+            return 'R$ 5,99'
+
         case 'laranja':
         case '2':
-            alert('7,99')
-            break;
+            return 'R$ 7,99'
 
         case 'sair':
         case '0':
-            alert('Programa encerrado')
-            continua = false
-            break;
+            return 'Programa encerrado'
 
         default:
-            alert('Produto não cadastrado.')
-            break;
+            return 'Produto não cadastrado.'
     }
 }
+
+// console.log('main.js')
+// Execução contínua só via JS
+// let continua = true;
+// while (continua) {
+//     const input = prompt('Digite nome ou código do produto. Digite 0 ou sair para encerrar')
+//     switch (input.toLocaleLowerCase()) {
+//         case 'morango':
+//         case '1':
+//             alert('R$ 5,99')
+//             break;
+//         case 'laranja':
+//         case '2':
+//             alert('7,99')
+//             break;
+
+//         case 'sair':
+//         case '0':
+//             alert('Programa encerrado')
+//             continua = false
+//             break;
+
+//         default:
+//             alert('Produto não cadastrado.')
+//             break;
+//     }
+// }
